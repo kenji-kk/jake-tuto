2ac4df32f98203c169a5c367ee22dc348517bb63
kenji <kenjix12x22@gmail.com>
2021-05-03 19:09:19 +0900

    chakra-uiでテーマ作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Button, ChakraProvider } from '@chakra-ui/react';
+import theme from "./theme/theme";
 
-const App: React.FC = () => {
-  const [count, setCount] = useState(1)
-
+export default function app() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>{count}</div>
-        <div>
-          <button onClick={() => setCount(count + 1)}>Increment</button>
-        </div>
-      </header>
-    </div>
-  )
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="teal">ボタン</Button>
+      <p>あああああああ</p>
+    </ChakraProvider>
+  );
 }
-
-export default App
