a891deaa967d63f8303a870b49a00fe6f57bdd00
kenji <kenjix12x22@gmail.com>
2021-05-04 15:20:21 +0900

    ヘッダールーティング

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,11 +1,17 @@
 import { Flex, Heading, Box, Link, useDisclosure} from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { memo, useCallback, VFC } from "react";
+import { useHistory } from "react-router-dom";
 import { MenuIconButton } from "../../atoms/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
 
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose} = useDisclosure();
+  const history = useHistory();
+  const onClickHome = useCallback(() => history.push("/home"), []);
+  const onClickUserManagement = useCallback(() => history.push("/home/user_management"), []);
+  const onClickSetting = useCallback(() => history.push("/home/setting"), []);
+
   return (
     <>
     <Flex
@@ -16,20 +22,26 @@ export const Header: VFC = memo(() => {
      justify="space-between"
      padding={{base: 3,md: 5 }}
      >
-        <Flex align="center" as="a" mr={8} _hover={{cursor: "pointer"}}>
+        <Flex align="center" as="a" mr={8} _hover={{cursor: "pointer"}} onClick={onClickHome}>
         <Heading as="h1" fontSize={{base: "md",md: "lg"}}> 
         ユーザー管理アプリ
         </Heading>
         </Flex>
         <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
         <Box pr={4}>
-            <Link>ユーザー一覧</Link>
+            <Link onClick={onClickUserManagement}>ユーザー一覧</Link>
           </Box>
-          <Link>設定</Link>
+          <Link onClick={onClickSetting}>設定</Link>
         </Flex>
         <MenuIconButton onOpen={onOpen}/>
     </Flex>
-    <MenuDrawer onClose={onClose} isOpen={isOpen}/>
+    <MenuDrawer 
+      onClose={onClose} 
+      isOpen={isOpen} 
+      onClickHome={onClickHome} 
+      onClickSetting={onClickSetting} 
+      onClickUserManagement={onClickUserManagement}
+    />
     </>
   )
   });
